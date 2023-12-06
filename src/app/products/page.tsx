dd3c331c8481c179d90c0bc11b6e3ab67341d1f6
Alexandre Augusto Fonseca <alexandralexandrefonsecach@gmail.com>
2023-12-06 20:50:57 -0300

    refactor: reorganize folder structure

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { Product } from "@/types/product";
 import { useProductData } from "../customHooks/useProductData";
-import ProductCard from "../components/ProductCard";
-import Container from "../components/Container";
-import ProductGrid from "../components/ProductGrid";
+import ProductCard from "../../components/ProductCard";
+import Container from "../../components/Container";
+import ProductGrid from "../../components/ProductGrid";
 
 export default function ProductPage() {
   const products: Product[] = useProductData();
