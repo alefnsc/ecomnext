e9e4a80605b6abcf899918b472af337bd274b5a7
alefnsca-temp <153950070+alefnsca-temp@users.noreply.github.com>
2024-01-19 18:41:03 -0300

    fix ignore admin route middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,4 @@ export default authMiddleware({
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-  ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/admin"],
 };
